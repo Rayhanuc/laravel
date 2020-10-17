3c96ec2179c69287e4b19d44c8b94919ddffc3f6
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-10-17 09:35:58 +0600

    37.21 - Checking logged in user from controller
    37.21 - Checking logged in user from controller

--- authdemo/routes/web.php
+++ authdemo/routes/web.php
@@ -21,6 +21,19 @@ Route::get('/auth', function () {
     return view('auth');
 });
 
+Route::get('/auth2', function () {
+    echo "Hello";
+})->middleware('auth');
+
+Route::get('/auth3', function () {
+    if(Auth::check()){
+        echo "Logged IN as ".Auth::user()->name;
+    }else {
+        echo "Hello Guest";
+    }
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/demo', [App\Http\Controllers\HomeController::class, 'demo']);
