65b06eadc6d00f89808f34abc18711ca7c31342a
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-10-17 08:18:19 +0600

    37.20 - Showing any content to logged in users and guest users in the blade file
    37.20 - Showing any content to logged in users and guest users in the blade file

---
+++ authdemo/resources/views/auth.blade.php
@@ -0,0 +1,11 @@
+@auth
+You are Logged In as User: {{ Auth::user()->name}}
+@endauth
+
+@guest
+Hello Guest
+@endguest
+
+<p>
+Common Section
+</p>
--- authdemo/routes/web.php
+++ authdemo/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/auth', function () {
+    return view('auth');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
