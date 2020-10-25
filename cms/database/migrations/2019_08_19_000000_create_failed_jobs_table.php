4c0961335c2aceb8baf7b8ca42fe6186d511ba5b
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-10-25 13:49:27 +0600

    Laravel cms project created with packages
    Laravel cms project
    ui project and auth
    laravel updated
    database, tables created and dummy data added

---
+++ cms/database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFailedJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid', 100)->unique();
+            $table->text('connection', 100);
+            $table->text('queue');
+            $table->longText('payload', 100);
+            $table->longText('exception', 100);
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('failed_jobs');
+    }
+}
